170100e0d5c404b24d30a96af31648635ff94fb2
THIAGOXF <64556498+THIAGOXF@users.noreply.github.com>
2022-10-07 14:45:21 -0300

    Add files via upload

---
+++ appdeliery/app/src/main/java/br/ulbra/appdeliery/form_login.java
@@ -0,0 +1,13 @@
+package br.ulbra.appdeliery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class form_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+    }
+}
